5b3941ab6d6ca000f6900a63bee8f82d7de90014
spoorthimahesh <spoorthimaheshbhat@gmail.com>
2026-05-28 23:58:01 +0530

    revision of ex_4.py

--- 01_fundamentals/CH-2 Operators and Expressions/Exercises/ex_1.py
+++ 01_fundamentals/CH-2 Operators and Expressions/Exercises/ex_1.py
@@ -10,10 +10,10 @@
 a = 15
 b = 4
 
-print(f"Addition: {a+b}")
-print(f"Subtraction: {a-b}")
-print(f"Multiplication: {a*b}")
-print(f"Division: {a/b}")
-print(f"Floor Division: {a//b}")
-print(f"Modulus: {a%b}")
-print(f"Exponent: {a**b}")
+print(f"Addition: {a + b}")
+print(f"Subtraction: {a - b}")
+print(f"Multiplication: {a * b}")
+print(f"Division: {a / b}")
+print(f"Floor Division: {a // b}")
+print(f"Modulus: {a % b}")
+print(f"Exponent: {a ** b}")
--- 01_fundamentals/CH-2 Operators and Expressions/Exercises/ex_4.py
+++ 01_fundamentals/CH-2 Operators and Expressions/Exercises/ex_4.py
@@ -1,22 +1,11 @@
-# Exercise 4 — Logical Operators
-# Create:
-# age
-# has_ticket
-#
-# Check:
-# if a person can enter a movie theater
-# condition:
-# age >= 18
-# AND has ticket
-
 age = int(input("Enter your age: "))
-has_ticket = input("Do you have the ticket? y/n: ")
+has_ticket = input("Do you have the ticket? y/n: ").lower()
 
-if has_ticket == "y" and age >= 18:
-    print("You are eligible to enter the theater")
-elif has_ticket == "n":
-    print("You are not eligible to enter the theater")
-elif has_ticket == "y" and age < 18:
-    print("You are not eligible to enter the theater")
-else:
+if has_ticket not in ["y", "n"]:
     print("Invalid Input")
+
+elif has_ticket == "y" and age >= 18:
+    print("You are eligible to enter the theater")
+
+else:
+    print("You are not eligible to enter the theater")
